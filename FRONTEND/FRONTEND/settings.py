66dd9b90873e1cd8ceaf88b04eb0575ae18de5ac
Mohan <mohankrishna28803@gmail.com>
2025-12-05 16:42:29 +0530

    adding render file

--- FRONTEND/FRONTEND/settings.py
+++ FRONTEND/FRONTEND/settings.py
@@ -113,8 +113,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-STATIC_URL = "static/"
-STATICFILES_DIRS  = [BASE_DIR / "sleep_app" / "static"]
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "sleep_app" / "static"
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 
 # Default primary key field type
