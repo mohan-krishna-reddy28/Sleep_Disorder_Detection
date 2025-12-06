b40ecb00c57cdd227748de66d71d5ecd12d1fa43
Mohan <mohankrishna28803@gmail.com>
2025-12-06 09:10:23 +0530

    commit

--- FRONTEND/FRONTEND/settings.py
+++ FRONTEND/FRONTEND/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = "django-insecure-zcbv9(=x58fjpdwh$w8eq=sq9dse98)&^nj-ofjb9j&=vdd#1l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
 
 
 # Application definition
@@ -49,7 +50,8 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "FRONTEND.urls"
+ROOT_URLCONF = "FRONTEND.FRONTEND.urls"
+
 
 TEMPLATES = [
     {
